ac6a983f550da9a0a6e3a50a8b58d316a8bdfebf
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-08-05 19:50:02 -0400

    Initial Commit

---
+++ FastTrack/FastTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackApp.swift
+//  FastTrack
+//
+//  Created by Jiaming Guo on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
